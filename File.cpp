0e08866fe110112a937727180e5b2e907eab8fde
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-02-04 23:44:17 +0100

    new class with file to inherit

---
+++ File.cpp
@@ -0,0 +1,5 @@
+#include "File.h"
+
+#include <iostream>
+
+using namespace std;
---
+++ File.h
@@ -0,0 +1,22 @@
+#ifndef FILE_H
+
+
+#define FILE_H
+#include <iostream>
+
+
+using namespace std;
+
+class File
+{
+
+};
+
+
+
+
+
+
+
+
+#endif // FILE_H
